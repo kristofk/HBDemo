64378e382f2d0402ad2af762c5e974635ea5a2a4
Kristof K <kocsis.kris@gmail.com>
2020-07-08 12:42:10 +0200

    Initial commit

---
+++ HBDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HBDemo
+//
+//  Created by Kristof Kocsis on 2020. 07. 07..
+//  Copyright © 2020. Kristof Kocsis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
